54f08539df5cbb3be6dd290972ee6d85e24c29b8
lenovo <ihozomerveille@gmail.com>
2026-03-10 10:52:05 +0200

    still editing

--- employee-directory/src/components/EmployeeCard.jsx
+++ employee-directory/src/components/EmployeeCard.jsx
@@ -3,7 +3,15 @@ function EmployeeCard({ employee }) {
   const { id, name, email, phone, website, company } = employee;
 
   return (
-    <div className="card">
+    <div
+      style={{
+        border: "1px solid #ddd",
+        padding: "20px",
+        borderRadius: "8px",
+        boxShadow: "0 2px 6px rgba(0,0,0,0.1)",
+        background: "white"
+      }}
+    >
       <h3>{name}</h3>
 
       <p><strong>ID:</strong> {id}</p>
